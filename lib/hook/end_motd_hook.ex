5a25aedecc1d43a7126fcb3214524e9b9db1d6db
Marc Addeo <marcaddeo@gmail.com>
2015-01-25 23:00:25 -0500

    Fix typo in EndMotdHook

--- lib/hook/end_motd_hook.ex
+++ lib/hook/end_motd_hook.ex
@@ -3,6 +3,6 @@ defmodule Hank.Hook.EndMotdHook do
   alias Hank.Client.State, as: Client
 
   def run(%Message{command: :"376"}, %Client{channels: channels}) do
-    Enum.map(channels, fn (channel) -> {:raw, "JOIN #{channels}"} end)
+    Enum.map(channels, fn (channel) -> {:raw, "JOIN #{channel}"} end)
   end
 end
