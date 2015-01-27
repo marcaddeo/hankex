98fcddd08c9595540cee42a1b4511ca3b2070c55
Marc Addeo <marcaddeo@gmail.com>
2015-01-26 21:45:13 -0500

    Fix error in connection's socket initialization. Deleted too much!

--- lib/connection.ex
+++ lib/connection.ex
@@ -9,7 +9,7 @@ defmodule Hank.Connection do
 
   def init(%State{} = state) do
     {:ok, socket} = Socket.TCP.connect(state.hostname, state.port, packet: :line)
-    {:ok, %State{socket: socket}, 0}
+    {:ok, %State{state | socket: socket}, 0}
   end
 
   def handle_info(:timeout, %State{client: client} = state) do
