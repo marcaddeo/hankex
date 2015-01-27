147522f348fdc25a5ce24d75a0cb34c0ab8e299c
Marc Addeo <marcaddeo@gmail.com>
2015-01-27 13:51:05 -0500

    Add some comments to the client

--- lib/client.ex
+++ lib/client.ex
@@ -4,7 +4,17 @@ defmodule Hank.Client do
   alias Hank.Connection
   alias Hank.Client.State, as: Client
 
+  @defmodule """
+  `Hank.Client` is the brains of the operation. It processes messages from the
+  IRC server and calls hooks if they match the command issued by the IRC server
+  and sends commands to the server depending on the response from the hook.
+  """
+
   defmodule State do
+    @defmodule """
+    The state of the client
+    """
+
     defstruct [
       connection:  nil,
       channels:    [],
@@ -15,6 +25,9 @@ defmodule Hank.Client do
     ]
   end
 
+  @doc """
+  Starts the GenServer for the client
+  """
   def start_link(%Client{} = client) do
     GenServer.start_link(__MODULE__, client)
   end
@@ -30,14 +43,24 @@ defmodule Hank.Client do
     {:noreply, client}
   end
 
+  @doc """
+  Loads a hook into the client while running
+  """
   def handle_cast({:load_hook, hook, function}, %Client{hooks: hooks} = client) do
     {:noreply, %Client{client | hooks: [{hook, function} | hooks]}}
   end
 
+  @doc """
+  Removes a hook from the client while running
+  """
   def handle_cast({:remove_hook, hook}, %Client{hooks: hooks} = client) do
     {:noreply, %Client{client | hooks: hooks -- hook}}
   end
 
+  @doc """
+  Parses IRC messages from the server and runs the respective hook for that IRC
+  command
+  """
   def handle_cast(%Message{command: hook} = message, %Client{hooks: hooks} = client) do
     if Keyword.has_key?(hooks, hook) do
       for function <- Keyword.get_values(hooks, hook) do
@@ -47,98 +70,160 @@ defmodule Hank.Client do
     {:noreply, client}
   end
 
+  @doc """
+  Joins a channel and updates the channel list in the client state
+  """
   def handle_cast({:join, channel}, %Client{connection: connection, channels: channels} = client) do
     Connection.send_message("JOIN #{channel}", connection)
     {:noreply, %Client{client | channels: [channel | channels]}}
   end
 
+  @doc """
+  Joins a password protected channel and updates the channel list in the client
+  state
+  """
   def handle_cast({:join, channel, key}, %Client{connection: connection, channels: channels} = client) do
     Connection.send_message("JOIN #{channel} #{key}", connection)
     {:noreply, %Client{client | channels: [channel | channels]}}
   end
 
+  @doc """
+  Parts a channel and updates the channel list in the client state
+  """
   def handle_cast({:part, channel}, %Client{connection: connection, channels: channels} = client) do
     Connection.send_message("PART #{channel}", connection)
     {:noreply, %Client{client | channels: channels -- channel}}
   end
 
+  @doc """
+  Parts a channel with a part message and updates the channel list in the client
+  state
+  """
   def handle_cast({:part, channel, message}, %Client{connection: connection, channels: channels} = client) do
     Connection.send_message("PART #{channel} :#{message}", connection)
     {:noreply, %Client{client | channels: channels -- channel}}
   end
 
+  @doc """
+  Changes the bots nickname, and updates the nickname in the client state
+  """
   def handle_cast({:nick, nickname}, %Client{connection: connection} = client) do
     Connection.send_message("NICK #{nickname}", connection)
     {:noreply, %Client{client | nickname: nickname}}
   end
 
+  @doc """
+  Sends a PRIVMSG message to target
+  """
   def handle_cast({:privmsg, target, message}, %Client{connection: connection} = client) do
     Connection.send_message("PRIVMSG #{target} :#{message}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Sends a CTCP message to target
+  """
   def handle_cast({:ctcp, target, message}, %Client{connection: connection} = client) do
     Connection.send_message("PRIVMSG #{target} :#{<<1, message, 1>>}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Sends an ACTION ctcp message to target
+  """
   def handle_cast({:action, target, message}, %Client{connection: connection} = client) do
     Connection.send_message("PRIVMSG #{target} :#{<<1, "ACTION ", message, 1>>}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Sends a NOTICE message to target
+  """
   def handle_cast({:notice, target, message}, %Client{connection: connection} = client) do
     Connection.send_message("NOTICE #{target} :#{message}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Sends the QUIT command to the server
+  """
   def handle_cast(:quit, %Client{connection: connection} = client) do
     Connection.send_message("QUIT :Leaving", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Sends a QUIT command to the server with a quit message
+  """
   def handle_cast({:quit, message}, %Client{connection: connection} = client) do
     Connection.send_message("QUIT :#{message}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Kicks the target from the channel
+  """
   def handle_cast({:kick, channel, target}, %Client{connection: connection} = client) do
     Connection.send_message("KICK #{channel} #{target}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Kicks the target from the channel with a kick message
+  """
   def handle_cast({:kick, channel, target, message}, %Client{connection: connection} = client) do
     Connection.send_message("KICK #{channel} #{target} :#{message}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Set the MODE of target to flags
+  """
   def handle_cast({:mode, target, flags}, %Client{connection: connection} = client) do
     Connection.send_message("MODE #{target} #{flags}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Set the MODE of target to flags with args
+  """
   def handle_cast({:mode, target, flags, args}, %Client{connection: connection} = client) do
     Connection.send_message("MODE #{target} #{flags} #{args}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Invite target to channel
+  """
   def handle_cast({:invite, target, channel}, %Client{connection: connection} = client) do
     Connection.send_message("INVITE #{target} #{channel}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Sends a PONG to the server with args
+  """
   def handle_cast({:pong, args}, %Client{connection: connection} = client) do
     Connection.send_message("PONG #{args}", connection)
     {:noreply, client}
   end
 
+  @doc """
+  Send a raw message to the server
+  """
   def handle_cast({:raw, message}, %Client{connection: connection} = client) do
     Connection.send_message(message, connection)
     {:noreply, client}
   end
 
+  @doc """
+  Handle a :noreply from a hook
+  """
   def handle_cast(:noreply, %Client{} = client), do: {:noreply, client}
 
+  @doc """
+  Iterate over a collection of hook responses and execute them
+  """
   def handle_cast(collection, %Client{} = client) do
     Enum.map(collection, fn (action) -> GenServer.cast(self, action) end)
     {:noreply, client}
