4e2f322de82a65d98a8c90314f56292e965ae56d
Marc Addeo <marcaddeo@gmail.com>
2015-02-02 12:37:04 -0500

    Refactor Connection Listener to have a name, and be stateless. It now gets the socket from the SocketAgent

--- lib/core/connection/connection_listener.ex
+++ lib/core/connection/connection_listener.ex
@@ -1,26 +1,28 @@
 defmodule Hank.Core.Connection.Listener do
   use GenServer
   require Logger
+  alias Hank.Core.Connection.SocketAgent
+  alias Hank.Core.Connection.Server, as: Client
 
-  def start_link(state) do
+  def start_link() do
     Logger.info("Starting Connection Listener")
-    GenServer.start_link(__MODULE__, state)
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def init(state) do
-    {:ok, state, 0}
+  def init(_) do
+    {:ok, [], 0}
   end
 
-  def handle_info(:timeout, state) do
-    listen(state)
-    {:noreply, state}
+  def handle_info(:timeout, _) do
+    listen(SocketAgent.get_socket())
+    {:noreply, []}
   end
 
-  defp listen({server, socket} = state) do
+  defp listen(socket) do
     case Socket.Stream.recv!(socket) do
       data when is_binary(data) ->
-        GenServer.cast(server, {:receive, data})
-        listen(state)
+        Client.receive(data)
+        listen(socket)
       nil ->
         :ok
     end
