cb51357c9cda096cab0a1383a844d9d1b53571c6
Marc Addeo <marcaddeo@gmail.com>
2015-02-02 12:46:47 -0500

    Start connection children in init callback

--- lib/core/connection/connection_sup.ex
+++ lib/core/connection/connection_sup.ex
@@ -7,18 +7,15 @@ defmodule Hank.Core.Connection.Supervisor do
 
   def start_link(%State{} = state) do
     Logger.info("Starting Connection Supervisor")
-    return = {:ok, sup} = Supervisor.start_link(__MODULE__, state, name: __MODULE__)
-    start_workers(sup, state)
-    return
+    Supervisor.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  def init(_) do
-    supervise([], strategy: :one_for_one)
-  end
+  def init(state) do
+    children = [
+      worker(Server, [state]),
+      worker(Listener, []),
+    ]
 
-  defp start_workers(sup, state) do
-    {:ok, server} = Supervisor.start_child(sup, worker(Server, [state]))
-    socket = GenServer.call(server, :socket)
-    Supervisor.start_child(sup, worker(Listener, [{server, socket}]))
+    supervise(children, strategy: :rest_for_one)
   end
 end
